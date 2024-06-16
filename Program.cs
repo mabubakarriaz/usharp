4bfc04467f45c36e44441644768b08221ed9962b
Abubakar Riaz <abubakar.riaz@outlook.com>
2024-06-16 22:27:16 +0500

    code files added

---
+++ Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.IO;
+
+class Preprocessor
+{
+    static void Main(string[] args)
+    {
+        if (args.Length != 2)
+        {
+            Console.WriteLine("Usage: Preprocessor <input file> <output file>");
+            return;
+        }
+
+        string inputFile = args[0];
+        string outputFile = args[1];
+
+        string[] lines = File.ReadAllLines(inputFile);
+        using (StreamWriter writer = new StreamWriter(outputFile))
+        {
+            foreach (string line in lines)
+            {
+                string translatedLine = TranslateLine(line);
+                writer.WriteLine(translatedLine);
+            }
+        }
+    }
+
+    static string TranslateLine(string line)
+    {
+        line = line.Replace("banao", "var");
+        line = line.Replace("agar", "if");
+        line = line.Replace("warna", "else");
+        line = line.Replace("likho", "Console.WriteLine");
+
+        return line;
+    }
+}
---
+++ translated.cs
@@ -0,0 +1,9 @@
+var x = 10;
+if (x > 5)
+{
+    Console.WriteLine("The X is greated than 5");
+}
+else
+{
+    Console.WriteLine("The X is less than 5");
+}
